9bc823b5816e5a3cd1812f2edca7634000b566a1
Александар Коцевски <alek.kocevski@hotmail.com>
2021-03-22 18:22:46 +0100

    *Created an About page using ViewModel

--- ContosoUniversity_AlekK/Controllers/HomeController.cs
+++ ContosoUniversity_AlekK/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ContosoUniversity_AlekK.DAL;
+using ContosoUniversity_AlekK.ViewModels;
 
 namespace ContosoUniversity_AlekK.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolContext db = new SchoolContext();
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to Contuso University";
@@ -16,9 +19,16 @@ namespace ContosoUniversity_AlekK.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            //ViewBag.Message = "Your application description page.";
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
 
-            return View();
+            return View(data.ToList());
         }
 
         public ActionResult Contact()
@@ -27,5 +37,11 @@ namespace ContosoUniversity_AlekK.Controllers
 
             return View();
         }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
     }
 }
