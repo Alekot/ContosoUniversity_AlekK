83dd633ccd9043d1c60808237d9882e28ef73a0e
Александар Коцевски <alek.kocevski@hotmail.com>
2021-03-25 20:19:45 +0100

    * Loading of related data (lazy/eager/explicit) * Page Courses (Controller and Index View) * Page Instructors (Controller with eager loading of navigation properties, View with a InstructorIndexData ViewModel)

---
+++ ContosoUniversity_AlekK/ViewModels/InstructorIndexData.cs
@@ -0,0 +1,15 @@
+﻿using ContosoUniversity_AlekK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity_AlekK.ViewModels
+{
+    public class InstructorIndexData
+    {
+        public  IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
